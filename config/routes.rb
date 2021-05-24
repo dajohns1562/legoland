44558c448436ed0a5140f7ff620e70c32593e22c
Qiong DAI(Nelly) <daiqiong524@outlook.com>
2021-05-24 17:40:56 +1000

    admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+ 
   resources :topics
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+    # Login Sessions
+ # Login Sessions
+  get '/login' => 'session#new'
+  post '/login' => 'session#login'
+  delete '/login' => 'session#destroy'
+
 end
